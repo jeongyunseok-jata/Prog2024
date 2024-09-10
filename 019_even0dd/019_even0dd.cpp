b3cc201cac665eb2728539feac5d80c53c4d10ff
jeongyunseok-jata <58690986+jeongyunseok-jata@users.noreply.github.com>
2024-09-10 14:50:10 +0900

    Add files via upload

---
+++ 019_even0dd/019_even0dd.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int a;
+
+	printf("숫자 입력 : ");
+	scanf_s("%d", &a);
+
+	//if (a % 2 == 1) printf("%d은 홀수입니다.\n",a);
+	//if (a % 2 == 0) printf("%d은 짝수입니다.\n",a);
+	if (a % 2 == 1) printf("%d은 홀수입니다.\n", a);
+	else printf("%d은 짝수입니다.\n",a);
+}
---
+++ 020_plusMinus/020_plusMinus.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("숫자 입력: ");
+
+    scanf_s("%d", &n);
+
+    if (n > 0) {
+        printf("%d은 양수입니다.\n",n);
+    }
+    else if (n == 0) {
+        printf("%d은 0입니다.\n",n);
+    }
+    else {
+        printf("%d은 음수입니다.\n",n);
+    }
+
+}
