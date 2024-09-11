5a65468fdfefc71bf7a9cefd917679e517654be5
jeongyunseok-jata <58690986+jeongyunseok-jata@users.noreply.github.com>
2024-09-11 14:27:15 +0900

    Add files via upload

---
+++ 027_3problem/027_3problem.cpp
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int n = 0;
+    int i = 1;
+    printf("n값을 입력해주세요 : ");
+    scanf_s("%d", &n);
+    while (i <= n) {
+        printf("*");
+        i++;
+    }
+    printf("\n");
+    i = 1;
+    while (i <= n) {
+        if (i % 2 != 0) {
+            printf("%d ",i);
+        }
+        i++;
+   }
+
+    printf("\n");
+    i = 1;
+    int sum = 0;
+    while (i <= n) {
+        sum += i;
+        i++;
+    }
+    printf("sum  = %d", sum);
+}
