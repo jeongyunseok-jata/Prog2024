18492fab53cae7b59615e295c0f6207227683388
jeongyunseok-jata <58690986+jeongyunseok-jata@users.noreply.github.com>
2024-09-12 10:45:38 +0900

    Add files via upload

---
+++ 03_21/03_21.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int n, sum1 = 0, sum2 = 0, sum3 = 0;
+
+    printf("n값을 입력해주세요 : ");
+    scanf_s("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 == 0)
+            sum1 += i;
+        else
+            sum2 += i;
+        sum3 += i;
+    }
+    printf("1 ~ %d 까지의 짝수 합 : %d\n", n, sum1);
+    printf("1 ~ %d 까지의 홀수 합 : %d\n", n, sum2);
+    printf("1 ~ %d 까지의 합 : %d", n, sum3);
+}
---
+++ 03_22/03_22.cpp
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("입력 : ");
+    scanf_s("%d", &n);
+    for (int i = 1; i <= 9; i++) {
+        printf("%dx%d=%d\n", n, i, n * i);
+    }
+}
